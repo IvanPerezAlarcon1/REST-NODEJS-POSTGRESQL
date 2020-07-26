0dea2667b1cef483e3084a092672620bfcdca221
ivan <ivan.perez.alarcon@utem.cl>
2020-07-26 18:41:40 -0400

    se ingresa la ruta put para actualizar registros

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@
 const {Router} = require('express');
 const router = Router(); //Router es un objeto que permite guardar las rutas de mi servidor
 
-const {getUsers, getUserById,createUser,deleteUser} = require('../controllers/index.controller')
+const {getUsers, getUserById,createUser,deleteUser, updateUser} = require('../controllers/index.controller')
 
-router.get('/users', getUsers);
+router.get('/users', getUsers); //listar todos los registros
 router.get('/users/:id', getUserById); //con :id, es como decirle que aquí va un parámetro
-router.post('/users', createUser);
-router.delete('/users/:id', deleteUser);
+router.post('/users', createUser); //crear dato
+router.put('/users/:id',updateUser); //actualizar dato segun id
+router.delete('/users/:id', deleteUser); //borrar registro segun id
 
 module.exports = router;//podemos exportar este archivo

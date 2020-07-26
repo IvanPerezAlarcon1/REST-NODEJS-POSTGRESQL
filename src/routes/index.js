65f53b30429c565acb3e291581f367e48c5ae0d9
ivan <ivan.perez.alarcon@utem.cl>
2020-07-26 12:46:33 -0400

    archivo donde se definen las rutas que tendra el servidor

---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+//en este archivo definimos las rutas que tendrá nuestro servidor
+const {Router} = require('express');
+const router = Router(); //Router es un objeto que permite guardar las rutas de mi servidor
+
+module.exports = router;//podemos exportar este archivo

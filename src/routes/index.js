4d89fa6d1e4b48a7ba679ef671845e8d68bcab6b
ivan <ivan.perez.alarcon@utem.cl>
2020-07-26 18:00:43 -0400

    se agregaron las rutas para gets,get, post, delete

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,11 @@
 const {Router} = require('express');
 const router = Router(); //Router es un objeto que permite guardar las rutas de mi servidor
 
-router.get('/users',(req,res) => {
-    res.send('Hello users');
-})
+const {getUsers, getUserById,createUser,deleteUser} = require('../controllers/index.controller')
+
+router.get('/users', getUsers);
+router.get('/users/:id', getUserById); //con :id, es como decirle que aquí va un parámetro
+router.post('/users', createUser);
+router.delete('/users/:id', deleteUser);
 
 module.exports = router;//podemos exportar este archivo

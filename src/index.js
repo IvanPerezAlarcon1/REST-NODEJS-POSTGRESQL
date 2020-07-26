d620a33878c30d457f1754ce792d6e457e84fa80
ivan <ivan.perez.alarcon@utem.cl>
2020-07-26 12:48:17 -0400

    Se llama al archivo index.js de routes, se establecen middlewares para envíos y recibimiento de datos

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express'); //permite iniciar el servidor
+const app = express();
+
+//middlewares
+app.use(express.json());//cada vez que desde una app cliente, envíen al servidor un dato en formato json, el servidor es capaz de entenderlo y convertirlo a objeto javascript
+app.use(express.urlencoded({extended: false}));//también debe poder entender datos que vienen desde formularios, permite procesar datos de un formulario y convertirlo a objeto javascript
+//el extended:false es para indicar que solo se admiten datos simples, no imagenes
+
+//routes
+app.use(require('./routes/index'));
+
+app.listen(3000);//que el servidor guardado en app, escuche en el puerto 3000
+console.log('Server on port 3000');//una vez inicia el servidor muestra mensaje por consola
